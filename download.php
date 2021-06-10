bdb08f04d8537d5fa7b44a4be43b605aab945228
Parjanya Modi <parjanya.cs19@bmsce.ac.in>
2021-06-10 10:34:03 +0530

    Updates

--- download.php
+++ download.php
@@ -23,16 +23,45 @@ VALUES ('$ticket_no', '$user_name', '$user_email', '$user_number', '$platform', 
 
 if ($conn->query($sql) === TRUE) {
 if ($user_system=='Windows'){
-header("Location:/win/certitude/download.php");
+  if ($platform=='Certitude'){
+    $name= '/win/certitude.zip';
+    download($name);
+  }
 }
 if ($user_system=='Non-Windows'){
-header("Location:/unix/certitude/download.php");
+  if ($platform=='Certitude'){
+    $name= '/unix/certitude.zip';
+    download($name);
+  }
 }
 
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
+
+
+function download($name) {
+  $file = $name;
+
+  if (file_exists($file)) {
+      header('Content-Description: File Transfer');
+      header('Content-Type: application/octet-stream');
+      header('Content-Disposition: attachment; filename='.basename($file));
+      header('Content-Transfer-Encoding: binary');
+      header('Expires: 0');
+      header('Cache-Control: must-revalidate');
+      header('Pragma: public');
+      header('Content-Length: ' . filesize($file));
+      ob_clean();
+      flush();
+      readfile($file);
+      exit;
+  }
+}
+
 $conn->close();
+
+
 
 exit();
 
